7f4d1b9c667c863e8d6cbeb161e54102fc4d63ee
wahdanda <wahgeming47@gmail.com>
2026-07-22 14:30:32 +0800

    fix: update base path

--- src/main.jsx
+++ src/main.jsx
@@ -7,4 +7,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+  <BrowserRouter basename="/portofolio">
+    <Routes>
+      {/* https://wahdanda.github.io/portofolio/ */}
+    </Routes>
+  </BrowserRouter>
 )
