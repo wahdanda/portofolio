ec3ff9c1897ac783914fd57dc3935478419877bd
wahdanda <wahgeming47@gmail.com>
2026-07-22 14:46:35 +0800

    14.47

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  <BrowserRouter basename="/portofolio">
+  <BrowserRouter basename="/portofolio/">
     <Routes>
       {/* https://wahdanda.github.io/portofolio/ */}
     </Routes>
