16458c0c565f2df300043dc0b66a068570c56513
wahdanda <wahgeming47@gmail.com>
2026-07-31 16:26:19 +0800

    16:26

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Icon } from "@iconify/react";
-import logo from "../assets/logo/logo1.webp"
+import logo from "../assets/logo/logo1.png"
 import GooeyNav from './GooeyNav'
 
 function Navbar() {
